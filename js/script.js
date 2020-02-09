9c978e991fc38c1a4298e1049c0e9c09e3bc1f06
Tiago Soriano <tiago.ninjarj@gmail.com>
2020-02-09 01:35:35 -0500

    Added the active class to the current page, and create the bookATable page

--- js/script.js
+++ js/script.js
@@ -1,31 +1,46 @@
 $(document).ready(function(){
     
+    $("#home").addClass("active");
+
     var main = $("main");
 
     $("#bookATable").click(function() { 
-        main.html("Here si going to be the code of each page");
+        main.load("bookATable.html", function(){
+            $("a").removeClass("active");
+        });
     });
     
     $("#about").click(function() {
         main.load("about.html", function(){
+            $("a").removeClass("active");
+            $("#about").addClass("active");
         });
     });
 
     $("#menu").click(function(){
         main.load("menu.html", function(){
+            $("a").removeClass("active");
+            $("#menu").addClass("active");
         });
     });
 
     $("#stories").click(function(){
         main.load("stories.html", function(){
+            $("a").removeClass("active");
+            $("#stories").addClass("active");
         });
     });
 
     $("#contact").click(function(){
         main.load("contact.html", function(){
+            $("a").removeClass("active");
+            $("#contact").addClass("active");
         });
     });
-
+    $("#home").click(function(){
+        $("a").removeClass("active");
+        $("#home").addClass("active");
+    });
 });
 
 
