25f43e0d6ad7f7fd34228f27b56e038bfe4a7232
Sergio Sanchez <sergio.sanchezs@ingenieros.com>
2020-03-13 09:07:00 -0400

    zoom implemented in the index page and adjusting the gallery carousel in the gallery page

--- js/gallery.js
+++ js/gallery.js
@@ -18,14 +18,7 @@ var jcarousel = $('.jcarousel');
 jcarousel
     .on('jcarousel:reload jcarousel:create', function () {
         var width = jcarousel.innerWidth();
-
-        if (width >= 600) {
-            width = width / 3;
-        } else if (width >= 350) {
-            width = width / 2;
-        }
-
-        jcarousel.jcarousel('items').css('width', width + 'px');
+        jcarousel.jcarousel('items').css('width', width + 'px')
     })
     .jcarousel({
         wrap: 'circular'
