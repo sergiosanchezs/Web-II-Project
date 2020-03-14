549cc5efc64b05027c270f49cbc106090340db40
Tiago Soriano <tiago.ninjarj@gmail.com>
2020-03-13 23:59:33 -0400

    fixed more bugs

--- js/about.js
+++ js/about.js
@@ -8,29 +8,29 @@ $(document).ready(function(){
         collapsible: true
       });
 
-    // //   var counter = function() {
+      var counter = function() {
 		
-	// 	$('#section-counter').waypoint( function( direction ) {
+		$('#section-counter').waypoint( function( direction ) {
 
-	// 		if( direction === 'down' && !$(this.element).hasClass('ftco-animated') ) {
+			if( direction === 'down' && !$(this.element).hasClass('ftco-animated') ) {
 
-	// 			var comma_separator_number_step = $.animateNumber.numberStepFactories.separator(',')
-	// 			$('.number').each(function(){
-	// 				var $this = $(this),
-	// 					num = $this.data('number');
-	// 					console.log(num);
-	// 				$this.animateNumber(
-	// 				  {
-	// 				    number: num,
-	// 				    numberStep: comma_separator_number_step
-	// 				  }, 7000
-	// 				);
-	// 			});
+				var comma_separator_number_step = $.animateNumber.numberStepFactories.separator(',')
+				$('.number').each(function(){
+					var $this = $(this),
+						num = $this.data('number');
+						console.log(num);
+					$this.animateNumber(
+					  {
+					    number: num,
+					    numberStep: comma_separator_number_step
+					  }, 7000
+					);
+				});
 				
-	// 		}
+			}
 
-	// 	} , { offset: '95%' } );
+		} , { offset: '95%' } );
 
-	// // }
-	// // counter();
+	}
+	counter();
 });
