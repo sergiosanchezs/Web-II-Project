4aea2d32f9ac8b5ab888a21d4cb9157d01ff9097
Sergio Henrique Galdino Pereira <sergiohgp@Sergios-MacBook-Pro.local>
2020-03-15 20:52:59 -0400

    Some changes in about page and contact page

--- js/about.js
+++ js/about.js
@@ -1,36 +1,38 @@
 $(document).ready(function(){
-    setTimeout(() => {  // After 1 second this will be executed!
-        $("#about_test").html("ABOUT is Executing properly!");
-    }, 1000);
+    // setTimeout(() => {  // After 1 second this will be executed!
+    //     $("#about_test").html("ABOUT is Executing properly!");
+    // }, 1000);
 
     $( "#tabs" ).tabs({
         event: "mouseover",
         collapsible: true
-      });
+	  });
+	  
+	//   $(".number").counterUp({delay: 10, time: 1000});
 
-      var counter = function() {
+    //   var counter = function() {
 		
-		$('#section-counter').waypoint( function( direction ) {
+	// 	$('#section-counter').waypoint( function( direction ) {
 
-			if( direction === 'down' && !$(this.element).hasClass('ftco-animated') ) {
+	// 		if( direction === 'down' && !$(this.element).hasClass('ftco-animated') ) {
 
-				var comma_separator_number_step = $.animateNumber.numberStepFactories.separator(',')
-				$('.number').each(function(){
-					var $this = $(this),
-						num = $this.data('number');
-						console.log(num);
-					$this.animateNumber(
-					  {
-					    number: num,
-					    numberStep: comma_separator_number_step
-					  }, 7000
-					);
-				});
+	// 			var comma_separator_number_step = $.animateNumber.numberStepFactories.separator(',')
+	// 			$('.number').each(function(){
+	// 				var $this = $(this),
+	// 					num = $this.data('number');
+	// 					console.log(num);
+	// 				$this.animateNumber(
+	// 				  {
+	// 				    number: num,
+	// 				    numberStep: comma_separator_number_step
+	// 				  }, 7000
+	// 				);
+	// 			});
 				
-			}
+	// 		}
 
-		} , { offset: '95%' } );
+	// 	} , { offset: '95%' } );
 
-	}
-	counter();
+	// }
+	// counter();
 });
