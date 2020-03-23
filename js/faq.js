19f245ae83495de61cfcd89591a82ecf391e5603
Tiago Soriano <tiago.ninjarj@gmail.com>
2020-03-23 02:04:48 -0400

    Added phone plugin

--- js/faq.js
+++ js/faq.js
@@ -10,33 +10,34 @@ $(document).ready(function(){
         $(this).removeAttr("rel");
     });
 
+    // Get all the innerText in the h3 tags inside the accordions
     var availableTags = $("#accordion h3, #accordion2 h3");
     var availableTagsArray = []
 
     for (var i = 0; i < availableTags.length; i++){
-        // each item (I need to append to an array)
+        // Append each h3 text to the array
         var eachQuestion = availableTags[i].innerText;
 
         availableTagsArray.push(eachQuestion);
     }
 
+    // Set the autocomplete widget
     $(".FAQsInput").autocomplete({
         source: availableTagsArray
     });
 
-    // Acordion
-
+    // Set the accordion widget
     $("#accordion, #accordion2").accordion({
         collapsible: true,
         active: 'none'
     });
 
     // Load the book a table page
-
     $("#bookATable").click(function() {
 		$("main").load("bookatable.html");
     });
 
+    // Load the contact page
     $(".contact-button").click(function() {
 		$("main").load("contact.html");
     });
