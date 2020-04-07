39b27bc9b64a9729696f1d405e4a2ac97523c09c
Sergio Pereira <sergiohgp@gmail.com>
2020-04-07 03:11:00 -0400

    Button mobile

--- js/mobile_app.js
+++ js/mobile_app.js
@@ -1,4 +1,10 @@
 $(document).ready(function(){
     $(".home-header h1").animate({ fontSize: "2.7rem" }, 1000 );
 
+    // Contact form validate mobile-contact-form
+    $( "#mobileModal" ).hide();
+
+    $("#btn-submit-mobile").on("click", function() {
+        $("#firstName").animate("bounce", { times: 3 }, "slow");
+     });
 });
